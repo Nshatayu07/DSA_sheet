2286e61b4e75de90c1bb1d5c17261281f91e5cc4
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-02-28 17:24:26 +0530

    Q added

---
+++ Linked_List/Segregate_even_and_odd_nodes_in_a_Linked_List.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+        // code here
+        Node* newhead = new Node(0);
+        Node* n1 = newhead;
+        Node* tailhead = new Node(0);
+        Node* n2 = tailhead;
+        Node* p = head;
+        while(p){
+            if(p->data % 2){
+                Node* n = new Node(p->data);
+                n2->next = n;
+                n2=n2->next;
+            }
+            else{
+                Node* n = new Node(p->data);
+                n1->next = n;
+                n1=n1->next;
+            }
+            p=p->next;
+        }
+        n1->next = tailhead->next;
+        return newhead->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
+
+// } Driver Code Ends
